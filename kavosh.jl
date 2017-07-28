cfd0417884ffe6e0740018302737d9c93d368092
Oliver Blanthorn <freedom4cows@gmail.com>
2017-07-28 11:28:30 +0100

    First working version.

--- kavosh.jl
+++ kavosh.jl
@@ -1,6 +1,6 @@
 # TODO:
 #   - give variables sensible names
-#   - Figure out where it returns
+#   - Figure out how to make it return
 #   - Multithreading...
 #       - Each root node is independent
 
@@ -11,6 +11,7 @@ const cb = Combinatorics
 
 # Find all subgraphs of size k in G
 function kavosh(G,k)
+    answers = []
     Visited = zeros(Bool,lg.nv(G))
     # For each node u
     for u in lg.vertices(G)
@@ -20,32 +21,36 @@ function kavosh(G,k)
         Visited[u] = true
         # S are parents?
         S[1] = u
-        Enumerate_Vertex(G,u,S,k-1,1,Visited)
+        Enumerate_Vertex(G,u,S,k-1,1,Visited,answers)
     end
+    answers
 end
 
 # Take graph, root vertex, "Selection": the vertices that are part of the current motif, the number of nodes left to choose
-function Enumerate_Vertex(G,u,S,Remainder,i,Visited)
+function Enumerate_Vertex(G,u,S,Remainder,i,Visited,answers)
     # If there are no more nodes to choose, terminate
+    s = deepcopy(S) # Stops shorter trees from accidentally sharing data. Must be a neater way of doing this.
     if Remainder == 0
         # TBH, this should probably return S.
-        temp = filter(x -> x!=0,vcat(values(S)...))
-        # Rarely, nodes are repeated. Why? Fixed - S wasn't being wiped.
-        arerepeats(temp) && println(S)
+        # Sometimes, there are too many nodes in the motif.
+        temp = vcat(values(s)...)
+        push!(answers,temp)
+        # Next step: olieshomegrowncannonlabeller(lg.adjacency_matrix(lg.induced_subgraph(G,temp)))
+        # oh god must I really nauty
         return
     else
         # Find vertices that could be part of unique motifs
-        ValList = Validate(G,S[i],u,Visited)
+        ValList = Validate(G,s[i],u,Visited)
         # Make sure we don't try to pick more than we can
         n = min(length(ValList),Remainder)
         # Pick k nodes from our current depth
         for k = 1:n
             for combination in cb.combinations(ValList,k)
-                # Set the current selection at the current depth to them nodes
-                # Might get a performance boost if S was just a list of numbers, and Parents was stored separately.
-                S[i+1] = combination
+                # set the current selection at the current depth to them nodes
+                # Might get a performance boost if s was just a list of numbers, and Parents was stored separately.
+                s[i+1] = combination
                 # and Remainder-k from other depths
-                Enumerate_Vertex(G,u,S,Remainder-k,i+1,Visited)
+                Enumerate_Vertex(G,u,s,Remainder-k,i+1,Visited,answers)
                 # Repeat for all combinations of k nodes at current depth
             end
         end
@@ -66,10 +71,11 @@ function Validate(G,Parents,u,Visited)
     ValList = []
     # For all of the immediate neighbours of the parents
     for v in Parents
-        # Original paper has this as the neighbours of U. 90% sure that it's a typo.
-        for w in lg.neighbors(G,v)
+        # Original paper has this as the neighbours of U. 100% sure that it's a typo.
+        for w in lg.all_neighbors(G,v)
             # If the label of the neighbour is greater than the parent, and the neighbour has not yet been visited,
-            if (v < w) && !Visited[w]
+            # I think perhaps it should be the root node, actually
+            if (u < w) && !Visited[w]
                 # Mark it as visited and add it to our list of candidates
                 Visited[w] = true
                 push!(ValList, w)
