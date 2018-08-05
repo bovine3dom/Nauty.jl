65180fa99d61c35a5d054d63386be17b2577c4b4
Oliver Blanthorn <freedom4cows@gmail.com>
2018-08-05 19:35:25 +0100

    Add benchmark to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 import Nauty
 import LightGraphs
+using BenchmarkTools
 const n = Nauty
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
@@ -7,12 +8,12 @@ else
     using Test
 end
 
+helper(x) = LightGraphs.Graph(x .| x')
+iso1a = helper(Array([0 1 1; 0 0 0; 0 0 0]))
 @testset begin
    "Convert the adjacency matrix of a directed graph into an undirected graph."
-   helper(x) = LightGraphs.Graph(x .| x')
 
    # Two simple isomorphic graphs.
-   iso1a = helper(Array([0 1 1; 0 0 0; 0 0 0]))
    iso1b = helper(Array([0 0 0; 1 0 1; 0 0 0]))
 
    @test n.lg_to_nauty(iso1a) == Array{UInt64,1}([0x6000000000000000, 0x8000000000000000, 0x8000000000000000])
@@ -28,4 +29,8 @@ end
    # This indirectly tests densenauty(), optionblk(), optionblk_mutable(), lg_to_nauty()
    @test n.canonical_form(iso1a).canong == n.canonical_form(iso1b).canong
    @test n.canonical_form(diso1a).canong == n.canonical_form(diso1b).canong
+
 end
+
+println("The following should take about 7 microseconds:")
+@show @benchmark n.canonical_form(iso1a).canong
