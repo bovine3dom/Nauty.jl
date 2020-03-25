a7091a63948b045be76f23be4638fddfb94a1c31
Oliver Blanthorn <freedom4cows@gmail.com>
2020-03-25 21:04:57 +0000

    Fix build on Julia 1

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-info("Building nauty...")
+@info "Building nauty..."
 nautydir = "$(@__DIR__)/nauty26r7/"
 nautyfiles = nautydir .* ["nauty","nautil","naugraph", "schreier","naurng"] .* ".c"
 run(`gcc -DWORDSIZE=64 -DMAXN=WORDSIZE -O4 -o $(@__DIR__)/minnautywrap.so $(@__DIR__)/minnautywrap.c $nautyfiles -shared -fPIC -I $nautydir`)
