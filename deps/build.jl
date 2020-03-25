e589746c1e94f4ef0c496061b5d87c7906644fb7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-03-25 19:41:50 -0400

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -2,14 +2,14 @@
 using Libdl
 
 function packagedir(pkg::AbstractString)
-	if endswith(pkg, ".jl")
-	   pkg = pkg[1:end-3]
-	end
-	pkgnautywrap = joinpath(depsdir, "minnautywrap." * Libdl.dlext) 
-  dir = Base.find_package(pkg)
-	isnothing(pkgdir) && throw(ErrorException("package \"$pkg\" not found"))              
-	pkgdir = abspath(joinpath(dirname(pkgdir), ".."))
-	return pkgdir
+    if endswith(pkg, ".jl")
+        pkg = pkg[1:end-3]
+    end
+    pkgnautywrap = joinpath(depsdir, "minnautywrap." * Libdl.dlext)
+    dir = Base.find_package(pkg)
+    isnothing(pkgdir) && throw(ErrorException("package \"$pkg\" not found"))              
+    pkgdir = abspath(joinpath(dirname(pkgdir), ".."))
+    return pkgdir
 end
 
 depsdir = joinpath(packagedir("Nauty"), "deps") 
